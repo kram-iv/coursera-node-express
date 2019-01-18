57a44e8d1b129b61bc7fbc7064d31861c283e4f7
kram-iv <vikram.karanth@gmail.com>
2019-01-18 17:19:36 +1100

    assignment 1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express     = require('express'),
       http        = require('http'),
       morgan      = require('morgan'),
-      bodyParser  = require('body-parser');
+      bodyParser  = require('body-parser'),
+      dishRouter  = require('./routes/dishRouter'),
+      promoRouter = require('./routes/promoRouter'),
+      leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,61 +12,15 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use( bodyParser.json() );
-
-
-app.all('/dishes', (req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/dishes', (req,res,next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post ('/dishes', (req,res,next) => {
-    res.end('Will add the dish: ' + req.body.name + 'with details ' + req.body.description);
-});
-
-app.put ('/dishes',(req,res,next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-app.delete ('/dishes', (req,res,next) => {
-    res.end('Deleting all dishes');
-});
-
-
-
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish! ' + 
-             req.params.dishId + ' to you!');
-});
-
-app.post ('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
-});
-
-app.put ('/dishes/:dishId',(req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + 
-            ' with details: ' + req.body.description);
-});
-
-app.delete ('/dishes/:dishId', (req,res,next) => {
-    res.end('Deleting dish ' + req.params.dishId);
-});
-
-
-
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
   console.log(req.headers);
+  console.log(req.url);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
